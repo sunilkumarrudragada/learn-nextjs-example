36f236117dedcbca31c81efc900fd190a1dca5c9
sunil7456 <sunilrudragada@gmail.com>
2024-07-19 15:58:46 +0530

    crud and lint

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -26,7 +26,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(event) => handleChanged(event.target.value)}
+        onChange={(event) =>  (event.target.value)}
         defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
